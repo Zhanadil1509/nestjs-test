a53241a77efbed91ddaa5f7ad130d29e2c04b068
Zhake <zhanadil1509@gmail.com>
2022-01-16 04:31:36 +0600

    removed left code, create user, install and setup swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,21 @@ import {
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module';
 import { join } from 'path'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageJson from '../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats description')
+    .setVersion(packageJson.version)
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   app.useStaticAssets({
     root: join(__dirname, '..', 'public'),
     prefix: '/public/',
@@ -21,6 +30,6 @@ async function bootstrap() {
     },
     templates: join(__dirname, '..', 'views')
   })
-  await app.listen(8000, '0.0.0.0');
+  await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
