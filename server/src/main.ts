405c41a56d85f5565634fbab707368cfde041c57
Zhake <zhanadil1509@gmail.com>
2022-01-16 15:15:29 +0600

    remove swagger, eslint, added tslint

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,21 +5,12 @@ import {
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module';
 import { join } from 'path'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as packageJson from '../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats description')
-    .setVersion(packageJson.version)
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api', app, document)
   app.useStaticAssets({
     root: join(__dirname, '..', 'public'),
     prefix: '/public/',
